b21e2f6f0cfa314b9fee59ad20cf1cb451213f33
sangram-dotnet-pune <sangramcanva7812@gmail.com>
2026-09-10 14:12:57 +0530

    fixed env var naming

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_GATEWAY || "http://localhost:5003",
+  baseURL: import.meta.env.API_GATEWAY || "http://localhost:5003",
   headers: {
     "Content-Type": "application/json",
   },
